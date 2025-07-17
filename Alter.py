1b050d74c1648a83f448acf9ae1c050c9e5d2ccf
sunny9n9 <itizsunny@outlook.com>
2025-07-18 02:20:43 +0530

    added structure to code
    made an object to do testing, though it felt like it should've been multiple objects, but we'll get to that if i stick long enough

--- Alter.py
+++ Alter.py
@@ -4,7 +4,7 @@ def PercentChange(OHLCV : pandas.DataFrame):
     TradingHour = ((OHLCV['Close'] - OHLCV['Open']) / OHLCV['Open']) * 100
     SinceLastClose = OHLCV["Close"].pct_change() * 100
 
-    return TradingHour    
+    return SinceLastClose    
 
 def MovingAvg(OHLCV, duration = 20, field = 'Close'):
     avgg = OHLCV[field].rolling(window=duration).mean()
